00f78e9749157b3304646a8b46a7d0ce29fd65a8
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-10-01 16:43:51 +0200

    expand test

--- tests/c/mem.c
+++ tests/c/mem.c
@@ -3,5 +3,11 @@
 int main(void) {
     char buf[10];
 
-    memset(buf, 0, sizeof(buf));
+    memset(buf, 34, sizeof(buf));
+
+    for (int i = 0; i < 10; ++i) {
+        if (buf[i] != 34) {
+            return 1;
+        }
+    }
 }
