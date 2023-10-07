2f52743d63d88aa3953342c872b8b963ab8e80c3
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-10-07 21:00:57 +0200

    like git submodules without the git submodules part

---
+++ tests/real_world/clone.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# It's like git submodules without the git submodules part.
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ensure_cloned() {
+    if [ ! -d "$1" ]; then
+        git clone "$1" "$SCRIPT_DIR/$2"
+    fi
+
+    (cd "$SCRIPT_DIR/$2" && git checkout "$3")
+}
+
+ensure_cloned "https://github.com/tsoding/noed.git" "tsoding_noed" "1c2bd182139080a8448a59589e8d457a7019d553"
